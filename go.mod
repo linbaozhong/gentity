e791d0cfe05476143d8ff31c2ea54665df351976
Harry Lin <lin.baozhong@hotmail.com>
2024-06-20 14:35:49 +0800

    增加脑图文档

--- go.mod
+++ go.mod
@@ -3,15 +3,26 @@ module gentity
 go 1.21.0
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gookit/slog v0.5.6
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/spf13/cobra v1.8.1
 	github.com/vetcher/go-astra v1.2.0
 	golang.org/x/tools v0.22.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/fatih/structtag v1.2.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/goutil v0.6.15 // indirect
+	github.com/gookit/gsr v0.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
---
+++ pkg/log/slog.go
@@ -0,0 +1,100 @@
+package log
+
+import (
+	"github.com/gookit/slog"
+	"github.com/gookit/slog/handler"
+	"github.com/gookit/slog/rotatefile"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var level slog.Level
+
+func RegisterLogger(production bool) {
+	_ = os.Mkdir("logs", os.ModePerm)
+
+	slog.Configure(func(l *slog.SugaredLogger) {
+		f := l.Formatter.(*slog.TextFormatter)
+		f.SetTemplate("{{datetime}} [{{level}}] [{{caller}}] {{message}}\n")
+		f.EnableColor = true
+		f.TimeFormat = time.DateTime
+		l.CallerSkip += 1
+
+		l.PushHandler(handler.MustFileHandler(filepath.Join(".", "logs", "error.log"),
+			handler.WithBuffMode(handler.BuffModeLine),
+			handler.WithRotateTime(rotatefile.EveryDay),
+			handler.WithLogLevels(slog.DangerLevels),
+		))
+
+		l.PushHandler(handler.MustFileHandler(filepath.Join(".", "logs", "info.log"),
+			handler.WithBuffMode(handler.BuffModeLine),
+			handler.WithRotateTime(rotatefile.EveryDay),
+			handler.WithLogLevels(slog.NormalLevels),
+		))
+	})
+	if production {
+		level = slog.ErrorLevel
+	} else {
+		level = slog.TraceLevel
+	}
+	slog.SetLogLevel(level)
+}
+
+func Trace(args ...any) {
+	slog.Trace(args...)
+}
+func Tracef(format string, args ...any) {
+	slog.Tracef(format, args...)
+}
+func Debug(args ...any) {
+	slog.Debug(args...)
+}
+func Debugf(format string, args ...any) {
+	slog.Debugf(format, args...)
+}
+func Info(args ...any) {
+	slog.Info(args...)
+}
+func Infof(format string, args ...any) {
+	slog.Infof(format, args...)
+}
+func Notice(args ...any) {
+	slog.Notice(args...)
+}
+func Noticef(format string, args ...any) {
+	slog.Noticef(format, args...)
+}
+func Warn(args ...any) {
+	slog.Warn(args...)
+}
+func Warnf(format string, args ...any) {
+	slog.Warnf(format, args...)
+}
+func Error(args ...any) {
+	slog.Error(args...)
+}
+func Errorf(format string, args ...any) {
+	slog.Errorf(format, args...)
+}
+
+func Fatal(args ...any) {
+	slog.Fatal(args...)
+}
+func Fatalf(format string, args ...any) {
+	slog.Fatalf(format, args...)
+}
+func Panic(args ...any) {
+	slog.Panic(args...)
+}
+func Panicf(format string, args ...any) {
+	slog.Panicf(format, args...)
+}
+
+func setLevel(l slog.Level) {
+	slog.SetLogLevel(l)
+}
+
+func Close() error {
+	return slog.Close()
+}
