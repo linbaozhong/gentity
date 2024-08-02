47135ec87e6b6583631def4be69dec974e6a6291
Harry Lin <lin.baozhong@hotmail.com>
2024-08-02 12:21:18 +0800

    init

--- pkg/ace/db.go
+++ pkg/ace/db.go
@@ -14,21 +14,21 @@ type (
 		QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 		ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	}
-	session struct {
+	DB struct {
 		*sqlx.DB
 	}
 )
 
-func Connect(driverName, dns string) (*session, error) {
+func Connect(driverName, dns string) (*DB, error) {
 	db, e := sqlx.Connect(driverName, dns)
 	if e != nil {
 		log.Panic(e)
 	}
-	return &session{db}, e
+	return &DB{db}, e
 }
 
 // Transaction 事务处理
-func (s *session) Transaction(ctx context.Context, f func(tx *sqlx.Tx) (interface{}, error)) (interface{}, error) {
+func (s *DB) Transaction(ctx context.Context, f func(tx *sqlx.Tx) (interface{}, error)) (interface{}, error) {
 	tx, e := s.BeginTxx(ctx, nil)
 	if e != nil {
 		return nil, e
