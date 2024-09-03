cba30a7fe337f3411b0fc0ffe97e0c9ed480bf7c
Harry Lin <lin.baozhong@hotmail.com>
2024-09-03 16:11:31 +0800

    init

--- example/dao/user_User_dao.go
+++ example/dao/user_User_dao.go
@@ -3,7 +3,6 @@ package dao
 import (
 	"context"
 	"github.com/linbaozhong/gentity/example/model"
-	// "github.com/linbaozhong/gentity/example/table/user"
 	"github.com/linbaozhong/gentity/pkg/ace"
 	atype "github.com/linbaozhong/gentity/pkg/ace/types"
 )
