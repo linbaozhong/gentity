8b90205f1873d2df022aef73d36582ad71bfc5cd
lin <lin.baozhong@hotmail.com>
2024-09-09 17:49:00 +0800

    init

--- internal/base/package.go
+++ internal/base/package.go
@@ -20,7 +20,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 )
 
 // DefaultConfig for loading Go base.
@@ -46,7 +45,6 @@ func PkgPath(config *packages.Config, target string) (string, error) {
 	// Try maximum 2 directories above the given
 	// target to find the root package or module.
 	var (
-		//n    = countLevels(target)
 		pkgs []*packages.Package
 	)
 
@@ -70,28 +68,4 @@ func PkgPath(config *packages.Config, target string) (string, error) {
 		return pkgs[0].PkgPath, nil
 	}
 	return "", fmt.Errorf("root package or module was not found for: %s", target)
-}
-
-func countLevels(p string) int {
-	// 使用Clean处理路径，确保其格式正确且符合当前系统
-	cleanPath := filepath.Clean(p)
-	// 获取系统相关的路径分隔符
-	separator := string(filepath.Separator)
-	// 分割路径，排除空字符串的情况
-	parts := strings.Split(cleanPath, separator)
-	count := 0
-	/// 开始计数前先检查是否以'.'开始，如果是，则先加1
-	if strings.HasPrefix(p, ".") {
-		count++ // 计入'.'作为一层
-	}
-	for _, part := range parts {
-		if part != "" {
-			count++
-		}
-	}
-	// 路径以分隔符结尾时调整层数
-	if cleanPath != separator && strings.HasSuffix(cleanPath, separator) {
-		count--
-	}
-	return count
 }
