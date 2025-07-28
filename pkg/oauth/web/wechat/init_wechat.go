7a1d3be896e8aca88d5332522de06f6a55b91906
Harry Lin <lin.baozhong@hotmail.com>
2025-07-28 11:02:04 +0800

    init

--- pkg/oauth/web/wechat/init_wechat.go
+++ pkg/oauth/web/wechat/init_wechat.go
@@ -6,19 +6,30 @@ import (
 	"github.com/linbaozhong/gentity/pkg/oauth/web"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
 	wxoption "github.com/wechatpay-apiv3/wechatpay-go/core/option"
-	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/h5"
 	"github.com/wechatpay-apiv3/wechatpay-go/utils"
 )
 
+type PaymentType string
+
+const (
+	PaymentTypeH5     PaymentType = "h5"
+	PaymentTypeNative PaymentType = "native"
+)
+
 type wx struct {
-	appid                      string
-	appSecret                  string
-	lang                       string
-	redirectURI                string // 回调地址
-	mchID                      string
-	mchCertificateSerialNumber string
-	mchPrivateKey              string
-	mchAPIv3Key                string
+	appid       string
+	appSecret   string
+	lang        string
+	redirectURI string // 授权回调地址
+	// 支付相关
+	mchID                      string      // 商户号
+	mchCertificateSerialNumber string      // 商户证书序列号
+	mchPrivateKey              string      // 商户私钥
+	mchAPIv3Key                string      // 商户APIv3密钥
+	mchPaymentType             PaymentType // 支付类型
+	// 通知相关
+	notifyURL string // 通知地址
+
 }
 type option func(w *wx)
 
@@ -29,27 +40,73 @@ const (
 	wechatUserInfoURL = "https://api.weixin.qq.com/sns/userinfo"
 )
 
+// WithAppId 设置appid
 func WithAppId(appid string) option {
 	return func(w *wx) {
 		w.appid = appid
 	}
 }
 
+// WithLang 设置语言
 func WithLang(lang string) option {
 	return func(w *wx) {
 		w.lang = lang
 	}
 }
 
+// WithAppSecret 设置appSecret
 func WithAppSecret(appSecret string) option {
 	return func(w *wx) {
 		w.appSecret = appSecret
 	}
 }
 
+// WithRedirectURI 设置授权回调地址
 func WithRedirectURI(redirectURI string) option {
 	return func(w *wx) {
 		w.redirectURI = redirectURI
+	}
+}
+
+// WithMchID 设置商户号
+func WithMchID(mchID string) option {
+	return func(w *wx) {
+		w.mchID = mchID
+	}
+}
+
+// WithMchCertificateSerialNumber 设置商户证书序列号
+func WithMchCertificateSerialNumber(mchCertificateSerialNumber string) option {
+	return func(w *wx) {
+		w.mchCertificateSerialNumber = mchCertificateSerialNumber
+	}
+}
+
+// WithMchPrivateKey 设置商户私钥
+func WithMchPrivateKey(mchPrivateKey string) option {
+	return func(w *wx) {
+		w.mchPrivateKey = mchPrivateKey
+	}
+}
+
+// WithMchAPIv3Key 设置商户APIv3密钥
+func WithMchAPIv3Key(mchAPIv3Key string) option {
+	return func(w *wx) {
+		w.mchAPIv3Key = mchAPIv3Key
+	}
+}
+
+// WithMchPaymentType 设置商户支付类型
+func WithMchPaymentType(mchPaymentType PaymentType) option {
+	return func(w *wx) {
+		w.mchPaymentType = mchPaymentType
+	}
+}
+
+// WithNotifyURL 设置通知地址
+func WithNotifyURL(notifyURL string) option {
+	return func(w *wx) {
+		w.notifyURL = notifyURL
 	}
 }
 
@@ -61,7 +118,7 @@ func New(opts ...option) web.Platformer {
 	return w
 }
 
-func (w *wx) client() *h5.H5ApiService {
+func (w *wx) client() *core.Client {
 	// 使用 utils 提供的函数从本地文件中加载商户私钥，商户私钥会用来生成请求的签名
 	mchPrivateKey, err := utils.LoadPrivateKey(w.mchPrivateKey)
 	if err != nil {
@@ -74,7 +131,6 @@ func (w *wx) client() *h5.H5ApiService {
 	if err != nil {
 		log.Fatal("new wechat client error")
 	}
-	return &h5.H5ApiService{
-		Client: _cli,
-	}
+
+	return _cli
 }
--- pkg/oauth/web/wechat/payment_wechat.go
+++ pkg/oauth/web/wechat/payment_wechat.go
@@ -6,10 +6,23 @@ import (
 	"github.com/linbaozhong/gentity/pkg/oauth/web"
 	"github.com/wechatpay-apiv3/wechatpay-go/core"
 	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/h5"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/native"
 	"time"
 )
 
+// PagePay 统一下单支付
 func (w *wx) PagePay(ctx context.Context, req *web.PagePayReq) (string, error) {
+	switch w.mchPaymentType {
+	case PaymentTypeH5:
+		return w.h5(ctx, req)
+	case PaymentTypeNative:
+		return w.native(ctx, req)
+	}
+	return "", errors.New("invalid payment type")
+}
+
+// h5 浏览器打开支付
+func (w *wx) h5(ctx context.Context, req *web.PagePayReq) (string, error) {
 	_trade := h5.PrepayRequest{}
 	_trade.OutTradeNo = core.String(req.Bill.String())
 	_trade.Amount = &h5.Amount{
@@ -25,7 +38,8 @@ func (w *wx) PagePay(ctx context.Context, req *web.PagePayReq) (string, error) {
 			req.Buyer.String() + web.Passbackchar +
 			req.Seller.String())
 
-	resp, result, err := w.client().Prepay(ctx, _trade)
+	_cli := &h5.H5ApiService{Client: w.client()}
+	resp, result, err := _cli.Prepay(ctx, _trade)
 	if err != nil {
 		return "", err
 	}
@@ -33,4 +47,32 @@ func (w *wx) PagePay(ctx context.Context, req *web.PagePayReq) (string, error) {
 		return "", errors.New(result.Response.Status)
 	}
 	return *resp.H5Url, nil
+}
+
+// native 扫码支付
+func (w *wx) native(ctx context.Context, req *web.PagePayReq) (string, error) {
+	_trade := native.PrepayRequest{}
+	_trade.OutTradeNo = core.String(req.Bill.String())
+	_trade.Amount = &native.Amount{
+		Total:    core.Int64(req.Amount.Int64()),
+		Currency: core.String(req.Currency),
+	}
+	_trade.Description = core.String(req.Desc)
+	_trade.NotifyUrl = core.String(req.NotifyUrl)
+	_trade.TimeExpire = core.Time(time.Now().Add(time.Minute * 30))
+	_trade.Attach = core.String(
+		req.Sku.String() + web.Passbackchar +
+			req.Sharer.String() + web.Passbackchar +
+			req.Buyer.String() + web.Passbackchar +
+			req.Seller.String())
+
+	_cli := &native.NativeApiService{Client: w.client()}
+	resp, result, err := _cli.Prepay(ctx, _trade)
+	if err != nil {
+		return "", err
+	}
+	if result.Response.StatusCode != 200 {
+		return "", errors.New(result.Response.Status)
+	}
+	return *resp.CodeUrl, nil
 }
