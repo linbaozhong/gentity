15ee1ae78e1e3e7e2004af813bb82f2e4e7f7df9
Harry Lin <lin.baozhong@hotmail.com>
2026-08-17 13:40:38 +0800

    init

--- example/tui/backend/internal/model/define/dao/user_dao_User.gen.go
+++ example/tui/backend/internal/model/define/dao/user_dao_User.gen.go
@@ -364,7 +364,9 @@ func (p *user) List(ctx context.Context, s ace.SelectBuilder) ([]*do.User, bool,
 	defer _rows.Close()
 
 	_obj := do.NewUser()
-	_objs, has, e := _obj.Scan(_rows, _cols...)
+	defer _obj.Free()
+
+	_objs, has, e := _obj.Slice(_rows, _cols...)
 	if has {
 		return _objs, true, nil
 	}
